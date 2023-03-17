84e160706b17de20348d0d9985ef35efe885a4e6
Chepkoech1 <sandrachepkoech60@gmail.com>
2023-03-18 01:32:22 +0300

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# Create some passengers
+passenger1 = Passenger.create(name: "Alice")
+passenger2 = Passenger.create(name: "Bob")
+passenger3 = Passenger.create(name: "Charlie")
+
+# Create some taxis
+taxi1 = Taxi.create(make: "Toyota", model: "Camry")
+taxi2 = Taxi.create(make: "Honda", model: "Civic")
+taxi3 = Taxi.create(make: "Ford", model: "Escape")
+
+# Create some rides
+Ride.create(passenger: passenger1, taxi: taxi1)
+Ride.create(passenger: passenger2, taxi: taxi1)
+Ride.create(passenger: passenger2, taxi: taxi2)
+Ride.create(passenger: passenger3, taxi: taxi3)
